b0310ab8a0e2e1e07d91c6c2f66f1c7d52accdc5
Louis Rebaud <louis.rebaud@gmail.com>
2023-09-04 17:04:36 +0200

    add setup.py for pip publication

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name='robi',
+    version='0.1.0',
+    description='ROBI: Robust and Optimized Biomarker Identifier',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Lrebaud/robi',
+    author='Louis Rebaud',
+    author_email='louis.rebaud@gmail.com',
+    license="Apache License 2.0",
+    packages=['icare'],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'seaborn',
+        'scikit-learn',
+        'lifelines',
+        'tqdm',
+        'statmodels',
+        'ray',
+        'multipy'
+    ],
+    tests_require=['torch', 'pytest'],
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
